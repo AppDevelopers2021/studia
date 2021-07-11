89fe97ac764a200498083d94b886d9ca9f05374e
Jongwoo Lee <jongwoo.developer@gmail.com>
2021-07-11 19:01:26 +0900

    embed login popup

--- public/calendar/index.js
+++ public/calendar/index.js
@@ -1,8 +1,8 @@
 // JS 코드는 추가해도 좋습니다.
 const popup = document.getElementsByClassName("popup")[0];
+let login_popup = document.getElementById("login-popup");
 var database = firebase.database();
 var uid;
-var signin;
 var setDate = new Date();
 
 document.getElementsByClassName('hamburger')[0].addEventListener("click", function () {
@@ -11,6 +11,16 @@ document.getElementsByClassName('hamburger')[0].addEventListener("click", functi
 
 document.getElementsByClassName('close_nav')[0].addEventListener("click", function () {
     document.getElementsByClassName('sidenav')[0].style.width = "0"
+});
+
+document.getElementById("login").addEventListener("click", function () {
+    login_popup.hidden = false;
+    login_popup.style.opacity = 1;
+});
+
+document.getElementById("login-close").addEventListener("click", function () {
+    login_popup.hidden = true;
+    login_popup.style.opacity = 0;
 });
 
 document.getElementById("add").addEventListener("click", function () {
@@ -46,7 +56,7 @@ firebase.auth().onAuthStateChanged((user) => {
 
         fetchData(getDate(setDate));
     } else {
-        // User is signed in
+        // User is not signed in
         signin = false;
     }
 })
@@ -70,3 +80,104 @@ function fetchData(date) {
 function addNote(note) {
     console.log(fetchData(setDate))
 }
+
+document.getElementById("confirm").addEventListener('click', function (evt) {
+    document.getElementById("confirm").style.cursor = "wait";
+
+    var email = document.getElementById("email").value;
+    var pw = document.getElementById("pw").value;
+    var keep = document.getElementById("keep").value;
+    var log = document.getElementById("error");
+    log.innerText = ''
+
+    // Set Persistence
+    if (keep) {
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+            .then(() => {
+                // Set to LOCAL
+                return firebase.auth().signInWithEmailAndPassword(email, pw)
+                    .then((userCredential) => {
+                        // Signed In
+                        var user = userCredential.user;
+                        login_popup.hidden = true;
+                        login_popup.style.opacity = 0;
+                    })
+                    .catch((error) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        console.error('Error while login: ' + errorCode + ' : ' +
+                            errorMessage)
+                        if (errorCode == "auth/wrong-password") {
+                            log.innerText = "비밀번호가 잘못되었습니다."
+                        } else if (errorCode == "auth/user-not-found") {
+                            log.innerText = "계정을 찾을 수 없습니다."
+                        } else if (errorCode == "auth/invalid-email") {
+                            log.innerText = "잘못된 이메일 주소입니다."
+                        } else {
+                            log.innerText = "에러가 발생하였습니다."
+                        }
+                    })
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.error('Error while setting persistence: ' + errorCode + ' : ' +
+                    errorMessage)
+            })
+    } else {
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+            .then(() => {
+                // Set to SESSION
+                return firebase.auth().signInWithEmailAndPassword(email, pw)
+                    .then((userCredential) => {
+                        // Signed In
+                        var user = userCredential.user;
+                        login_popup.hidden = true;
+                        login_popup.style.opacity = 0;
+                    })
+                    .catch((error) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        console.error('Error while login: ' + errorCode + ' : ' +
+                            errorMessage)
+                        if (errorCode == "auth/wrong-password") {
+                            log.innerText = "비밀번호가 잘못되었습니다."
+                        } else if (errorCode == "auth/user-not-found") {
+                            log.innerText = "계정을 찾을 수 없습니다."
+                        } else if (errorCode == "auth/invalid-email") {
+                            log.innerText = "잘못된 이메일 주소입니다."
+                        } else {
+                            log.innerText = "에러가 발생하였습니다."
+                        }
+                    })
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.error('Error while setting persistence: ' + errorCode + ' : ' +
+                    errorMessage)
+            })
+    }
+    document.getElementById("confirm").style.cursor = "none";
+
+});
+
+// Login with google
+document.getElementById("google").addEventListener("click", function () {
+    var log = document.getElementById("error");
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            var credential = result.credential;
+            var token = credential.accessToken;
+            var user = result.user;
+            console.log('login conplete')
+        }).catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            log.innerText = "Error " + errorCode + ": " + errorMessage;
+        })
+});
