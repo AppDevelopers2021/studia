b2e46cd358833833c0b34a6639afc5d98429bb25
이종우 <jongwoo.developer@gmail.com>
2021-10-21 07:48:25 +0900

    Add iforgot page

---
+++ public/iforgot/index.js
@@ -0,0 +1,44 @@
+const hamburger = document.getElementsByClassName("nav_hamburger")[0];
+const navbar = document.getElementById("navbar");
+const sidenav = document.getElementById("sidenav");
+const close_nav_button = document.getElementById("close_nav");
+
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("bar_open");
+    sidenav.classList.toggle("side_open");
+})
+close_nav_button.addEventListener("click", function () {
+    hamburger.className = "nav_hamburger center";
+    sidenav.className = "";
+})
+
+const button = document.getElementById("iforgot_button");
+const loader = document.getElementById("iforgot_loader");
+const email = document.getElementById("iforgot_id");
+
+function shakeInput(elem) {
+    elem.classList.add("iforgot_error");
+}
+
+button.addEventListener("click", function () {
+    loader.classList.add("loading");
+    firebase.auth().sendPasswordResetEmail(email.value)
+        .then(() => {
+            // Email is sent
+            loader.classList.remove("loading");
+        })
+        .catch((error) => {
+            // Error
+            switch (error.code) {
+                case "auth/user-not-found":
+                    shakeInput(email);
+                    break;
+                case "auth/invalid-email":
+                    shakeInput(email);
+                    break;
+                default:
+                    console.error(`Error while sending Password Reset Email :: ${error.code} : ${error.message}`);
+            }
+            loader.classList.remove("loading")
+        })
+})
